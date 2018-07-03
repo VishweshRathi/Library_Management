385482e85a50a4f9026a8a8eda4ad1c1aa8dfa9f
Vishwesh Rathi <vishweshsr@gmail.com>
2018-07-03 20:23:25 +0530

    Initial commit

---
+++ client/webpack.config.js
@@ -0,0 +1,34 @@
+var config = {    
+    entry : './src/index.jsx',
+    output: {
+        path: __dirname,
+        filename: 'index.min.js'
+    },
+    devServer: {
+        inline :true,
+        port: 8081
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(png|jpg)$/,
+                loader: 'url?limit=25000',
+                test: /\.jsx?$/,
+                loader:    'babel-loader',
+                query: {
+                    presets: ['es2016', 'react', 'stage-2']
+                },
+            },
+            {
+                test: /\.css$/,
+                loaders: ['style-loader','css-loader' ],
+            }
+        ]
+    },
+    node: {
+            fs: 'empty',
+            net: 'empty'
+    }
+};
+
+module.exports = config;
